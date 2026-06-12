026d78b1d28ed7eaf7d4a19fe754e85bd92b2ac3
manichinniah06 <manichinniah94@gmail.com>
2026-06-12 10:20:07 +0530

    display greeting

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Hello, World! This is a simple JavaScript file.");
+
+const greeting = "Welcome to JavaScript programming!";
+function displayGreeting() {
+  console.log(greeting);
+}
+displayGreeting();
